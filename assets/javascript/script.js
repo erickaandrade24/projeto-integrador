0a4920e4600a05571d45f9625df192c74b6c6923
erick alves <erick.andrade240499@gmail.com>
2025-04-29 04:18:36 -0300

    adicionando cores e um pouco de js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,14 @@
 document.getElementById('open_uni').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 
+
 });
+
+function mostrarTela(){
+   const secoes =  document.querySelectorAll('main section').forEach(secao =>{secao.style.display= 'none'});
+
+   const ativa = document.getElementById('tela-' +id);
+   if(ativa) ativa.style.display = 'block';
+   
+}
+
